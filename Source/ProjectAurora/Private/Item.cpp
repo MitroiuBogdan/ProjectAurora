24834bbee465772494b62c97db911fb2bc50ff21
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-01-14 22:47:38 +0200

    partial fix collitin

--- Source/ProjectAurora/Private/Item.cpp
+++ Source/ProjectAurora/Private/Item.cpp
@@ -6,15 +6,16 @@ AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	ItemMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
-	RootComponent = ItemMeshComponent;
 	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
-	SphereComponent->SetupAttachment(GetRootComponent());
+	SphereComponent->SetupAttachment(ItemMeshComponent);
+	
 }
 
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AItem::OverlapWithSphere);
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnBeginOverlap);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &AItem::OnEndOverlap);
 }
 
 void AItem::Tick(float DeltaTime)
@@ -22,10 +23,21 @@ void AItem::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
-void AItem::OverlapWithSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                              UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-                              const FHitResult& SweepResult)
+void AItem::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                           UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                           const FHitResult& SweepResult)
 {
-	FString OtherActorName = OtherActor->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("OverlapWithSphere - %p"), OtherActor)
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,TEXT("OnBeginOverlap"));
+	}
+}
+
+void AItem::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                         int32 OtherBodyIndex)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,TEXT("OnEndOverlap"));
+	}
 }
--- Source/ProjectAurora/Public/Item.h
+++ Source/ProjectAurora/Public/Item.h
@@ -26,7 +26,11 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION()
-	virtual void OverlapWithSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	                               UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-	                               const FHitResult& SweepResult);
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                    const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
