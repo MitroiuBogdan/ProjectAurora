478cee8e1d349bd886aecc790563bb9a060c9472
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-01-15 20:04:43 +0200

    Attach weapons

--- Source/ProjectAurora/Private/Sword.cpp
+++ Source/ProjectAurora/Private/Sword.cpp
@@ -3,13 +3,23 @@
 
 #include "Sword.h"
 
+#include "Slash.h"
+
 void ASword::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                            const FHitResult& SweepResult)
 {
 	Super::OnBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,TEXT("SWORD"));
+	}
+
+	ASlash* Slash = Cast<ASlash>(OtherActor);
+	if (Slash)
+	{
+		FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget,true);
+		ItemMeshComponent->AttachToComponent(Slash->GetMesh(), TransformRules, FName("hand_r_socket"));
 	}
 }
 
--- Source/ProjectAurora/Public/Item.h
+++ Source/ProjectAurora/Public/Item.h
@@ -14,23 +14,22 @@ class PROJECTAURORA_API AItem : public AActor
 public:
 	AItem();
 
-	UPROPERTY(VisibleAnywhere)
+protected:
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	class UStaticMeshComponent* ItemMeshComponent;
 	UPROPERTY(VisibleAnywhere)
 	class USphereComponent* SphereComponent;
-
-protected:
-	virtual void BeginPlay() override;
 
 public:
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION()
 	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-	                    const FHitResult& SweepResult);
+	                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                            const FHitResult& SweepResult);
 
 	UFUNCTION()
 	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
