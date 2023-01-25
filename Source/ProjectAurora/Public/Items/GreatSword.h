afb4f798f29bca8ec9428d4c8eae7d23a93cbf43
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-01-25 20:37:51 +0200

    Add GreatSword.cpp

---
+++ Source/ProjectAurora/Public/Items/GreatSword.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/BaseWeapon.h"
+#include "GreatSword.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTAURORA_API AGreatSword : public ABaseWeapon
+{
+	GENERATED_BODY()
+	
+};
